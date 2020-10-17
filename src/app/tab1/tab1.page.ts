f029c8b6e74293d515d3ed20303efe9d2b0876b6
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-10-17 19:07:45 -0300

    Logic fixed

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor() { } 
+  numero: string = null;
+  base: string;
+  n_base: string;
+  vl_base: number;
+  vl_n_base: number;  
+  converter() {
+    switch (this.n_base) {
+      case 'bin':
+        this.vl_n_base = 2;
+        break;
+      case 'dec': 
+        this.vl_n_base = 10;
+        break;
+      case 'oct': 
+        this.vl_n_base = 8;
+        break;
+      case 'hex': 
+        this.vl_n_base = 16;
+        break;
+      default:
+        console.error(this.base);
+    }
+    let numeroValor: any = Number(this.numero);
+    let numeroNovo: string;
+    numeroNovo = numeroValor.toString(this.vl_n_base);
+    console.log(numeroNovo)
+  }
 }
