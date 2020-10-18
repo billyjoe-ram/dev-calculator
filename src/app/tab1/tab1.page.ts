b66de18286e8904d8402302043d76d29286f8919
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-10-17 23:00:43 -0300

    new conversions added

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,16 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() { } 
+  constructor(public alertController: AlertController) { } 
   numero: string = null;
   base: string;
   n_base: string;
   vl_base: number;
-  vl_n_base: number;  
-  converter() {
+  vl_n_base: number;      
+  async resultado() {
     switch (this.n_base) {
-      case 'bin':
-        this.vl_n_base = 2;
+      case 'bin':    
+        this.vl_n_base = 2;    
         break;
       case 'dec': 
         this.vl_n_base = 10;
@@ -31,8 +32,32 @@ export class Tab1Page {
         console.error(this.base);
     }
     let numeroValor: any = Number(this.numero);
-    let numeroNovo: string;
-    numeroNovo = numeroValor.toString(this.vl_n_base);
-    console.log(numeroNovo)
+    let numeroNovo: any;
+    switch (this.base) {
+      case 'bin':
+        this.vl_base = 2;                
+        break;
+      case 'dec': 
+        this.vl_base = 10;        
+        break;
+      case 'oct': 
+        this.vl_base = 8;        
+        break;
+      case 'hex': 
+        this.vl_base = 16;        
+        break;
+      default:
+        console.error(this.base);
+    }
+    numeroNovo = Number.parseInt(numeroValor, this.vl_n_base);
+    console.log(numeroNovo);
+    const alert = await this.alertController.create({
+      cssClass: 'resultado',
+      header: 'Resultado',      
+      message: `${numeroNovo}`,
+      buttons: ['Muito Obrigado!']
+    });
+
+    await alert.present();
   }
 }
