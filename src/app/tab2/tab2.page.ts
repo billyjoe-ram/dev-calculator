602de9320dc945a170439acebeb7b0a6f118ca83
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-10-18 23:12:44 -0300

    Main color structure built

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,43 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
+  red: string;
+  green: string;
+  blue: string;  
+  async cor() {    
+    // Variáveis para o input e exibição
+    let pRed = this.red;
+    let pGreen = this.green;
+    let pBlue = this.blue;
+    let cor = pRed + pGreen + pBlue;
+    let corNome;
+    
+    // Cores padrão
+    switch (cor) {
+      case 'ff0000':    
+        corNome = 'vermelho';
+        break;
+      case '00ff00': 
+        corNome = 'verde';
+        break;
+      case '0000ff': 
+        corNome = 'azul';
+        break;      
+      default:
+        console.error(cor);
+        break;
+    }        
+
+    // Exibir a cor em um alert
+    const alert = await this.alertController.create({
+      cssClass: 'cor-alert',
+      header: 'Resultado',      
+      message: `A cor selecionada foi ${corNome}`,
+      buttons: ['Muito Obrigado!']
+    });
+    console.log(cor);
+    await alert.present();
+  }
 
 }
